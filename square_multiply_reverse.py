d2c2824fe239ae233687325d1eb5e1f625e078b7
Youheng Lü <youheng.lue@gmail.com>
2023-03-16 16:31:56 +0100

    [Feature] Batch experiments for instruction tracing

--- square_multiply_reverse.py
+++ square_multiply_reverse.py
@@ -4,6 +4,7 @@ import pathlib
 import os
 import pickle
 import claripy
+import click
 
 import spysgx
 PARENT_PATH = pathlib.Path(__file__).parent
@@ -13,14 +14,44 @@ PICKLE_PATH = PARENT_PATH / "cache" / "square_multiply.pickle"
 
 logging.getLogger("spysgx").setLevel(logging.DEBUG)
 
-if os.path.exists(PICKLE_PATH):
-    with open(PICKLE_PATH, "rb") as f:
-        proj = pickle.load(f)
-else:
-    proj = spysgx.Project(ENCLAVE_PATH, "sgx_mod_exp", PICKLE_PATH)
 
-# Reversing
-secret = claripy.BVS("secret", 64)
-proj.guard.simgr.active[0].regs.rsi = secret
-state = spysgx.reverse(proj.guard.simgr, TRACEFILE) 
-print("Secret =", state.solver.eval(secret, cast_to=int))
+@click.command()
+@click.option("--tracefolder", default=PARENT_PATH / "traces" / "square_multiply", help="The trace folder that contains the secrets.")
+@click.option("--results", default=None, help="The file to write the results to. If none is given then the results will not be printed.")
+@click.option("--debug", is_flag=True, help="Enable debug mode.")
+def argparse(tracefolder, results, debug):
+    """Parse the command line arguments."""
+    if debug:
+        logging.getLogger("spysgx").setLevel(logging.DEBUG)
+    else:
+        logging.getLogger("spysgx").setLevel(logging.INFO)
+    tracefolder = pathlib.Path(tracefolder)
+
+    # Get all the trace files form the trace folder
+    tracefiles = [tracefile for tracefile in tracefolder.iterdir() if tracefile.is_file()]
+    print(tracefiles)
+
+    for i, tracefile in enumerate(tracefiles, 1):
+        print(f"Tracing secret {i}/{len(tracefiles)}")
+        if os.path.exists(PICKLE_PATH):
+            with open(PICKLE_PATH, "rb") as f:
+                proj = pickle.load(f)
+        else:
+            proj = spysgx.Project(ENCLAVE_PATH, "sgx_mod_exp", PICKLE_PATH)
+
+        # Set the secret value to a symbolic variable
+        secret = claripy.BVS("secret", 64)
+        proj.guard.simgr.active[0].regs.rsi = secret
+        state = spysgx.reverse(proj.guard.simgr, tracefile)
+        secret = state.solver.eval(secret, cast_to=int)
+
+        if results is not None:
+            with open(results, "a+") as f:
+                f.write(f"{tracefile.name}, {secret}\n")
+        
+        print(f"{tracefile.name}, {secret}")
+        # import IPython; IPython.embed()
+
+# Start reversing
+if __name__ == "__main__":
+    argparse()
--- square_multiply_trace.py
+++ square_multiply_trace.py
@@ -4,27 +4,51 @@ import pathlib
 import pickle
 import os
 import sys
+import random
+import click
 
 import spysgx.project
-SECRET = 1337
-
-if len (sys.argv) > 1:
-    SECRET = int(sys.argv[1])
 
 PARENT_PATH = pathlib.Path(__file__).parent
+
 ENCLAVE_PATH = PARENT_PATH / "enclaves" / "square_multiply.signed.so"
 PICKLE_PATH = PARENT_PATH / "cache" / "square_multiply.pickle"
-TRACEFILE = PARENT_PATH / "traces" / "square_multiply" / "trace.txt"
 
-logging.getLogger("spysgx").setLevel(logging.DEBUG)
+@click.command()
+@click.option("--secret", default=1337, help="The secret value to trace.")
+@click.option("--tracefolder", default=PARENT_PATH / "traces" / "square_multiply", help="The trace folder to write to.")
+@click.option("--num", default=1, help="The number of traces to generate. If this number is > 1 then the secret will be generated randomly.")
+@click.option("--debug", is_flag=True, help="Enable debug mode.")
+def argparse(secret, tracefolder, num, debug):
+    """Parse the command line arguments."""
+    if debug:
+        logging.getLogger("spysgx").setLevel(logging.DEBUG)
+    else:
+        logging.getLogger("spysgx").setLevel(logging.INFO)
+        
+    tracefolder = pathlib.Path(tracefolder)
+    tracefolder.mkdir(parents=True, exist_ok=True)
+
+    if num > 1:
+        secrets = [random.randint(0, 2**30) for _ in range(num)]
+    else:
+        secrets = [secret]
+
+    for i, secret in enumerate(secrets, 1):
+        print(f"Tracing secret {i}/{num}")
+        tracefile = tracefolder / f"trace_{secret}.txt"
+        proj.guard.simgr.active[0].regs.rsi = secret
+        spysgx.trace(proj.guard.simgr, tracefile)
 
 # Load the project from a pickle file if it exists
-if os.path.exists(PICKLE_PATH):
-    with open(PICKLE_PATH, "rb") as f:
-        proj = pickle.load(f)
-else:
-    proj = spysgx.Project(ENCLAVE_PATH, "sgx_mod_exp", PICKLE_PATH)
-# Try to reach the mod_exp function
-# Set the secret value
-proj.guard.simgr.active[0].regs.rsi = SECRET
-spysgx.trace(proj.guard.simgr, TRACEFILE)
+if __name__ == "__main__":
+    if os.path.exists(PICKLE_PATH):
+        with open(PICKLE_PATH, "rb") as f:
+            proj = pickle.load(f)
+    else:
+        proj = spysgx.Project(ENCLAVE_PATH, "sgx_mod_exp", PICKLE_PATH)
+    # Try to reach the mod_exp function
+    
+    # Start tracing
+    argparse()
+
